65a9384f1eabccffa8fbc8ef6d718cffb5150999
rajesh123rai <rajes1173@gmail.com>
2026-02-28 19:33:14 +0530

    New changes

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,13 +1,16 @@
 package com.example.demo;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
+
 @RestController
 public class HelloController {
     @GetMapping("/hello")
     public String sayHello(@RequestParam(defaultValue = "Stranger") String name) {
         return "Hello, " + name + "!";
     }
+
     @GetMapping("/bye")
     public String sayBye(@RequestParam(defaultValue = "Stranger") String name) {
         return "Bye, " + name + "!";
