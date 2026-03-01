4620c41d29d4ca029a21fa92f74410330ef7e759
rajesh123rai <rajes1173@gmail.com>
2026-03-01 15:23:35 +0530

    New data added to UserController

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,4 +1,7 @@
 package com.example.demo;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +12,14 @@ public class UserController {
     public User getUser() {
         User user = new User(1, "Rahul", "Kozhikode");
         return user;
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        List<User> users = new ArrayList<>();
+        users.add(new User(1, "Rahul", "Kozhikode"));
+        users.add(new User(2, "John", "Bangalore"));
+        users.add(new User(3, "Sara", "Mumbai"));
+        return users;
     }
 }
