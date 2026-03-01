390d2a8af51f2b1f9ed31e2f534c6e552cc5b3ac
rajesh123rai <rajes1173@gmail.com>
2026-03-01 15:09:55 +0530

    New files to Create new API's

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+public class User {
+    private int id;
+    private String name;
+    private String city;
+
+    // Constructor
+    public User(int id, String name, String city) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+    }
+
+    // Getters
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getCity() { return city; }
+}
