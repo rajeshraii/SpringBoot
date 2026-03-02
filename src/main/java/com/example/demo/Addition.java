0b21ec0fae7761ccfbde8c0e67f68fd726276bea
rajesh123rai <rajes1173@gmail.com>
2026-03-02 18:20:46 +0530

    New file Addition is added

---
+++ src/main/java/com/example/demo/Addition.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+public class Addition {
+    private int num1;
+    private int num2;
+
+    public Addition(int num1,int num2){
+        this.num1=num1;
+        this.num2=num2;
+    }
+    public int sum(){
+        return num1+num2;
+    }
+    
+}
