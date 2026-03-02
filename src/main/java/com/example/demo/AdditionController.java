de5787f4f22b6588616d51bf47c6ef4446be1206
rajesh123rai <rajes1173@gmail.com>
2026-03-02 19:12:13 +0530

    new file added

--- src/main/java/com/example/demo/AdditionController.java
+++ src/main/java/com/example/demo/AdditionController.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +13,16 @@ public class AdditionController {
     public Addition addData(){
         Addition add = new Addition(10,30);
         return add;
+    }
+
+    @GetMapping("/addmore")
+    public List<Addition> getMoreData(){
+        List<Addition> addmore = new ArrayList<>();
+        addmore.add(new Addition(40,90));
+        addmore.add(new Addition(60,100));
+        addmore.add(new Addition(1000,100000));
+        addmore.add(new Addition(75,85));
+        addmore.add(new Addition(98,187));
+        return addmore;
     }
 }
