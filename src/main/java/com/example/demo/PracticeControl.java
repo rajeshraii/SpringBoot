99cbd8c8b4c1673ef269997d32d205f96d061dfa
rajesh123rai <rajes1173@gmail.com>
2026-03-03 22:29:33 +0530

    New file Practice is added

--- src/main/java/com/example/demo/PracticeControl.java
+++ src/main/java/com/example/demo/PracticeControl.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +9,11 @@ import java.util.ArrayList;
 @RestController
 public class PracticeControl {
     @GetMapping("/practice")
-    public List<Practice> getPractice(){
-        List<Practice> practices=new ArrayList<>();
-        practices.add(new Practice("Rajesh",20,90));
-        practices.add(new Practice("Bob",25,89));
-        practices.add(new Practice("Prakash",20,35));
+    public List<Practice> getPractice() {
+        List<Practice> practices = new ArrayList<>();
+        practices.add(new Practice("Rajesh", 20, 90));
+        practices.add(new Practice("Bob", 25, 89));
+        practices.add(new Practice("Prakash", 20, 35));
         return practices;
     }
 }
-
