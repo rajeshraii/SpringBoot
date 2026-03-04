6e963fb9caed403bed5f31364ec68f0f0afd851c
rajesh123rai <rajes1173@gmail.com>
2026-03-04 22:38:31 +0530

    Learning Post

--- src/main/java/com/example/demo/PracticeControl.java
+++ src/main/java/com/example/demo/PracticeControl.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -15,5 +17,10 @@ public class PracticeControl {
         practices.add(new Practice("Bob", 25, 89));
         practices.add(new Practice("Prakash", 20, 35));
         return practices;
+    }
+
+    @PostMapping("/practice")
+    public Practice newPractice(@RequestBody Practice practice){
+        return practice;
     }
 }
