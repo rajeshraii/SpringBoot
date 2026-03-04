71720f0fdaf3381244defef0a9add5411b7b17f6
rajesh123rai <rajes1173@gmail.com>
2026-03-04 22:51:44 +0530

    Learning Post

--- src/main/java/com/example/demo/PracticeControl.java
+++ src/main/java/com/example/demo/PracticeControl.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import org.apache.logging.log4j.internal.annotation.SuppressFBWarnings;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
