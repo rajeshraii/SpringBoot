ce27b7c5e1841529d5ed4aec7858d655b1db68f2
rajesh123rai <rajes1173@gmail.com>
2026-03-05 20:32:56 +0530

    New Employee file is added

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+public class Employee {
+    private String name;
+    private int id;
+    private int salary;
+    private String result;
+
+    public Employee(String name,int id,int salary){
+        this.name=name;
+        this.id=id;
+        this.salary=salary;
+
+    if(salary>50000){
+        this.result="High payning job";
+    }
+    else{
+        this.result="Lower Salary";
+    }
+}
+    public String getName(){
+        return name;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public int getSalary(){
+        return salary;
+    }
+
+    public String getResult(){
+        return result;
+    }
+
+}
