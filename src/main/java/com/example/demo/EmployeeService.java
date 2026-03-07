5a9d7034a8585a177a23ddcd908179e768030869
rajesh123rai <rajes1173@gmail.com>
2026-03-07 19:38:09 +0530

    The Delete Method is added

--- src/main/java/com/example/demo/EmployeeService.java
+++ src/main/java/com/example/demo/EmployeeService.java
@@ -10,11 +10,25 @@ public class EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    //POST METHOD TO ADD EMPLOYEE
     public Employee addEmployee(Employee employee) {
         return employeeRepository.save(employee);
     }
 
+    //GET METHOD TO GET ALL EMPLOYEES
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
+
+    //GET METHOD TO GET ONE EMPLOYEE BY ID
+    public Employee getEmployeeById(int id){
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    //DELETE METHOD TO DELETE EMPLOYEE BY ID
+    public String deleteEmployeeById(int id){
+        employeeRepository.deleteById(id);
+        return "The Employee with id "+id+ "has been deleted";
+    }
+    
 }
