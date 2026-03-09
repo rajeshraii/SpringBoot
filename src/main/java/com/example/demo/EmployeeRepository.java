366389c8bf8e52de5ce9697dd491306ad30ecdad
rajesh123rai <rajes1173@gmail.com>
2026-03-09 22:22:10 +0530

    New changes

--- src/main/java/com/example/demo/EmployeeRepository.java
+++ src/main/java/com/example/demo/EmployeeRepository.java
@@ -2,8 +2,9 @@ package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
+    List<Employee> findBySalaryBetween(double min, double max);
 }
