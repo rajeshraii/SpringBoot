e1a7305c50960421f5bcdc202abcd80f54878782
rajesh123rai <rajes1173@gmail.com>
2026-03-09 22:26:23 +0530

    New changes

--- src/main/java/com/example/demo/EmployeeController.java
+++ src/main/java/com/example/demo/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -10,39 +11,50 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-    //POST METHOD API
+    // POST METHOD API
     @PostMapping("/employees")
     public Employee addEmployee(@RequestBody Employee employee) {
         return employeeService.addEmployee(employee);
     }
 
-    //GET METHOD API
+    // GET METHOD API
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
         return employeeService.getAllEmployees();
     }
 
-    //GET METHOD API FOR ONE ID
+    // GET METHOD API FOR ONE ID
     @GetMapping("/employees/{id}")
-    public Employee getEmployeeById(@PathVariable int id){
+    public Employee getEmployeeById(@PathVariable int id) {
         return employeeService.getEmployeeById(id);
     }
 
-    //UPDATE METHOD API
+    // UPDATE METHOD API
     @PutMapping("/employees/{id}")
-    public Employee updateEmployee(@PathVariable int id,@RequestBody Employee employee){
-        return employeeService.updateEmployee(id,employee);
+    public Employee updateEmployee(@PathVariable int id, @RequestBody Employee employee) {
+        return employeeService.updateEmployee(id, employee);
     }
 
-    //DELETE API
+    // DELETE API
     @DeleteMapping("/employees/{id}")
-    public String deleteEmployeeById(@PathVariable int id){
+    public String deleteEmployeeById(@PathVariable int id) {
         return employeeService.deleteEmployeeById(id);
     }
 
-    //PATCH UPDATE API
+    // PATCH UPDATE API
     @PatchMapping("/employees/{id}/salary")
-    public Employee updateSalary(@PathVariable int id,@RequestParam double salary){
-        return employeeService.updateSalary(id,salary);
+    public Employee updateSalary(@PathVariable int id, @RequestParam double salary) {
+        return employeeService.updateSalary(id, salary);
     }
+
+
+    // API FOR SALARY RANGE
+    @GetMapping("/employees/salary")
+    public ResponseEntity<List<Employee>> getBySalaryRange(
+            @RequestParam double min,
+            @RequestParam double max) {
+        return ResponseEntity.ok(
+                employeeService.getEmployeesBySalaryRange(min, max));
+    }
+
 }
