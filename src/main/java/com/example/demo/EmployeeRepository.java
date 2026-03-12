e713b791e3dc16b32ed35e3b191e9dbd88c0d27f
rajesh123rai <rajes1173@gmail.com>
2026-03-12 23:01:41 +0530

    New Repo command for new API

--- src/main/java/com/example/demo/EmployeeRepository.java
+++ src/main/java/com/example/demo/EmployeeRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     List<Employee> findBySalaryBetween(double min, double max);
     List<Employee> findByNameContaining(String name);
+    List<Employee> findByHighPaying();
 }
