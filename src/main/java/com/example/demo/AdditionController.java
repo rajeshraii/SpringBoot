ab6995b2261f5a2eedc3f99f7d64a921a3cefd95
rajesh123rai <rajes1173@gmail.com>
2026-03-20 23:14:49 +0530

    Some Changes

--- src/main/java/com/example/demo/AdditionController.java
+++ src/main/java/com/example/demo/AdditionController.java
@@ -8,21 +8,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AdditionController {
-    
+
     @GetMapping("/add")
-    public Addition addData(){
-        Addition add = new Addition(10,30);
+    public Addition addData() {
+        Addition add = new Addition(10, 30);
         return add;
     }
 
     @GetMapping("/addmore")
-    public List<Addition> getMoreData(){
+    public List<Addition> getMoreData() {
         List<Addition> addmore = new ArrayList<>();
-        addmore.add(new Addition(40,90));
-        addmore.add(new Addition(60,100));
-        addmore.add(new Addition(1000,100000));
-        addmore.add(new Addition(75,85));
-        addmore.add(new Addition(98,187));
+        addmore.add(new Addition(40, 90));
+        addmore.add(new Addition(60, 100));
+        addmore.add(new Addition(1000, 100000));
+        addmore.add(new Addition(75, 85));
+        addmore.add(new Addition(98, 187));
         return addmore;
     }
 }
